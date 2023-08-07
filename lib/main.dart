abad51c233f6b2a882370d6cc27b481d5575dbeb
yudystriawan <yudistriawan@gmail.com>
2023-08-07 19:25:24 +0700

    setup firebase core

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:chat_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // setup firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
