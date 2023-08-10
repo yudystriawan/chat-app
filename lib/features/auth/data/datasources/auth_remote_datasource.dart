03001b08b580184512da2a02ceb00bf3f0083c03
yudystriawan <yudistriawan@gmail.com>
2023-08-10 12:46:01 +0700

    add auth remote data source

---
+++ lib/features/auth/data/datasources/auth_remote_datasource.dart
@@ -0,0 +1,29 @@
+import 'package:chat_app/core/errors/failure.dart';
+import 'package:chat_app/core/services/firebase_services/auth_service.dart';
+
+import '../models/model.dart';
+
+abstract class AuthRemoteDatasource {
+  Future<UserDto?> loginWithGoogle();
+}
+
+class AuthFirebaseDatasource implements AuthRemoteDatasource {
+  final AuthService _service;
+
+  AuthFirebaseDatasource(this._service);
+
+  @override
+  Future<UserDto?> loginWithGoogle() async {
+    try {
+      final user = await _service.loginWithGoogle();
+
+      if (user == null) return null;
+
+      return UserDto.fromFirebase(user);
+    } on Failure {
+      rethrow;
+    } catch (e) {
+      throw const Failure.unexpectedError();
+    }
+  }
+}
