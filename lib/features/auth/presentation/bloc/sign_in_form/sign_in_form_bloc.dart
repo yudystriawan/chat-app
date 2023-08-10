9a48437267dbf6eff0c976cc5a6f8295802c861d
yudystriawan <yudistriawan@gmail.com>
2023-08-10 12:54:07 +0700

    add bloc

---
+++ lib/features/auth/presentation/bloc/sign_in_form/sign_in_form_bloc.dart
@@ -0,0 +1,37 @@
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../../../../core/errors/failure.dart';
+import '../../../../../core/usecases/usecase.dart';
+import '../../../domain/usecases/login_with_google.dart';
+
+part 'sign_in_form_bloc.freezed.dart';
+part 'sign_in_form_event.dart';
+part 'sign_in_form_state.dart';
+
+class SignInFormBloc extends Bloc<SignInFormEvent, SignInFormState> {
+  final LoginWithGoogle _loginWithGoogle;
+  SignInFormBloc(
+    this._loginWithGoogle,
+  ) : super(SignInFormState.initial()) {
+    on<_SignInWithGooglePressed>(_onSignInWithGooglePressed);
+  }
+
+  void _onSignInWithGooglePressed(
+    _SignInWithGooglePressed event,
+    Emitter<SignInFormState> emit,
+  ) async {
+    emit(state.copyWith(
+      isSubmitting: true,
+      failureOrSuccessOption: none(),
+    ));
+
+    final failureOrSuccess = await _loginWithGoogle(const NoParams());
+
+    emit(state.copyWith(
+      isSubmitting: false,
+      failureOrSuccessOption: optionOf(failureOrSuccess),
+    ));
+  }
+}
