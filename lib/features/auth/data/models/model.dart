d6937f27eb0eadd9e74e67df3e03d58c8ce58d62
yudystriawan <yudistriawan@gmail.com>
2023-08-10 12:46:14 +0700

    add user model

---
+++ lib/features/auth/data/models/model.dart
@@ -0,0 +1,8 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:firebase_auth/firebase_auth.dart' as f_auth;
+
+import '../../domain/entities/user.dart';
+
+part 'model.freezed.dart';
+part 'model.g.dart';
+part 'user.dart';
