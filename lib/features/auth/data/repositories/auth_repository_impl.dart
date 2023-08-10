737a3a87724c94a6a7b0c3293b4ebc0748343df4
yudystriawan <yudistriawan@gmail.com>
2023-08-10 12:46:25 +0700

    implement auth repo

--- lib/features/auth/data/repositories/auth_repository_impl.dart
+++ lib/features/auth/data/repositories/auth_repository_impl.dart
@@ -1,11 +1,27 @@
 import 'package:chat_app/core/errors/failure.dart';
+import 'package:chat_app/features/auth/data/datasources/auth_remote_datasource.dart';
 import 'package:chat_app/features/auth/domain/repositories/auth_repository.dart';
 import 'package:dartz/dartz.dart';
 
+import '../../domain/entities/user.dart';
+
 class AuthRepositoryImpl implements AuthRepository {
+  final AuthRemoteDatasource _remoteDatasource;
+
+  AuthRepositoryImpl(this._remoteDatasource);
+
   @override
-  Future<Either<Failure, Unit>> loginWithGoogle() {
-    // TODO: implement loginWithGoogle
-    throw UnimplementedError();
+  Future<Either<Failure, User>> loginWithGoogle() async {
+    try {
+      final user = await _remoteDatasource.loginWithGoogle();
+
+      if (user == null) return left(const Failure.canceledByUser());
+
+      return right(user.toDomain());
+    } on Failure catch (e) {
+      return left(e);
+    } catch (e) {
+      return left(const Failure.unexpectedError());
+    }
   }
 }
