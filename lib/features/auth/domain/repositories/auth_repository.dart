3c31219b080f762837384ca9becf385714d3453e
yudystriawan <yudistriawan@gmail.com>
2023-08-10 12:46:45 +0700

    update return type

--- lib/features/auth/domain/repositories/auth_repository.dart
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -1,7 +1,8 @@
 import 'package:dartz/dartz.dart';
 
 import '../../../../core/errors/failure.dart';
+import '../entities/user.dart';
 
 abstract class AuthRepository {
-  Future<Either<Failure, Unit>> loginWithGoogle();
+  Future<Either<Failure, User>> loginWithGoogle();
 }
--- lib/features/auth/domain/usecases/login_with_google.dart
+++ lib/features/auth/domain/usecases/login_with_google.dart
@@ -3,12 +3,14 @@ import 'package:chat_app/core/usecases/usecase.dart';
 import 'package:chat_app/features/auth/domain/repositories/auth_repository.dart';
 import 'package:dartz/dartz.dart';
 
-class LoginWithGoogle implements Usecase<Unit, NoParams> {
+import '../entities/user.dart';
+
+class LoginWithGoogle implements Usecase<User, NoParams> {
   final AuthRepository _repository;
 
   LoginWithGoogle(this._repository);
   @override
-  Future<Either<Failure, Unit>> call(NoParams params) async {
+  Future<Either<Failure, User>> call(NoParams params) async {
     return await _repository.loginWithGoogle();
   }
 }
