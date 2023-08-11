a39574bbae13149c9f1e55f2af6c2ac0288cfb8a
yudystriawan <yudistriawan@gmail.com>
2023-08-12 06:25:10 +0700

    remove di initialization

--- core/lib/core.dart
+++ core/lib/core.dart
@@ -1,5 +1,10 @@
 library core;
 
+import 'package:injectable/injectable.dart';
+
 export 'utils/di/injection.dart';
 export 'utils/errors/failure.dart';
 export 'utils/routes/router.dart';
+
+@InjectableInit.microPackage()
+void initMicroPackage() {}
--- core/lib/utils/di/injection.dart
+++ core/lib/utils/di/injection.dart
@@ -1,15 +1,3 @@
 import 'package:get_it/get_it.dart';
-import 'package:injectable/injectable.dart';
 
-import 'injection.config.dart';
-
-late GetIt getIt;
-
-@InjectableInit(
-  asExtension: false,
-  preferRelativeImports: true,
-)
-void configureDependencies(GetIt instance) {
-  init(instance);
-  getIt = instance;
-}
+GetIt getIt = GetIt.instance;
