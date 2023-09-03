aea3a07cf73790cc88ec8c31d2fd7d3c23e44ee1
yudystriawan <yudistriawan@gmail.com>
2023-09-04 06:20:26 +0800

    add addContact usecase

---
+++ lib/features/contacts/domain/usecases/add_contact.dart
@@ -0,0 +1,29 @@
+import 'package:chat_app/features/contacts/domain/repositories/contact_repository.dart';
+import 'package:core/utils/errors/failure.dart';
+import 'package:core/utils/usecases/usecase.dart';
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:injectable/injectable.dart';
+
+@injectable
+class AddContact implements Usecase<Unit, AddContactParams> {
+  final ContactRepository _repository;
+
+  AddContact(this._repository);
+
+  @override
+  Future<Either<Failure, Unit>> call(params) {
+    return _repository.addContact(params.username);
+  }
+}
+
+class AddContactParams extends Equatable {
+  final String username;
+
+  const AddContactParams({
+    required this.username,
+  });
+
+  @override
+  List<Object> get props => [username];
+}
