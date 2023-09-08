83d81e1f3f07d7aa548f85150bd5903c90eb7023
yudystriawan <yudistriawan@gmail.com>
2023-09-08 17:56:25 +0800

    expose field value

--- core/lib/firestore/firestore.dart
+++ core/lib/firestore/firestore.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:injectable/injectable.dart';
 
+export 'package:cloud_firestore/cloud_firestore.dart' show FieldValue;
+
 @injectable
 class FirestoreService {
   final FirebaseFirestore _firestore;
