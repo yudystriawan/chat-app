bfdcc66a925cd2873133e6f0d538a5aff0c22220
yudystriawan <yudistriawan@gmail.com>
2023-09-09 05:27:20 +0800

    add member dto

---
+++ lib/features/chat/data/models/member_dtos.dart
@@ -0,0 +1,27 @@
+import 'package:chat_app/features/chat/domain/entities/entity.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'member_dtos.freezed.dart';
+part 'member_dtos.g.dart';
+
+@freezed
+class MemberDto with _$MemberDto {
+  const MemberDto._();
+  const factory MemberDto({
+    String? name,
+    String? imageUrl,
+    String? id,
+  }) = _MemberDto;
+
+  factory MemberDto.fromJson(Map<String, dynamic> json) =>
+      _$MemberDtoFromJson(json);
+
+  Member toDomain() {
+    final empty = Member.empty();
+    return Member(
+      name: name ?? empty.name,
+      imageUrl: imageUrl ?? empty.imageUrl,
+      id: id ?? empty.id,
+    );
+  }
+}
