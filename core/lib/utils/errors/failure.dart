ea47e8265ce14293d8c8d72139c7088cb7fb4301
yudystriawan <yudistriawan@gmail.com>
2023-09-10 00:13:56 +0800

    add failure dat

--- core/lib/utils/errors/failure.dart
+++ core/lib/utils/errors/failure.dart
@@ -9,4 +9,5 @@ class Failure with _$Failure {
   const factory Failure.unexpectedError() = _UnexpectedError;
   const factory Failure.canceledByUser() = _CanceledByUser;
   const factory Failure.unauthenticated() = _Unauthenticated;
+  const factory Failure.notFound() = _NotFound;
 }
