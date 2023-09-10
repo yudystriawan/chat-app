5ce968c448ac8c7c30f7cdcba2276750b9752e16
yudystriawan <yudistriawan@gmail.com>
2023-09-10 14:05:07 +0800

    add bloc

--- lib/features/chat/presentation/pages/room_page.dart
+++ lib/features/chat/presentation/pages/room_page.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:chat_app/features/chat/presentation/blocs/member_watcher/member_watcher_bloc.dart';
 import 'package:chat_app/features/chat/presentation/blocs/messages_watcher/messages_watcher_bloc.dart';
 import 'package:chat_app/features/chat/presentation/blocs/room_watcher/room_watcher_bloc.dart';
 import 'package:chat_app/shared/app_bar.dart';
@@ -11,6 +12,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:kt_dart/collection.dart';
 
 import '../blocs/message_form/message_form_bloc.dart';
 import '../widgets/chats_container.dart';
@@ -26,22 +28,37 @@ class RoomPage extends StatelessWidget implements AutoRouteWrapper {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<MessageFormBloc, MessageFormState>(
-      listenWhen: (p, c) =>
-          p.failureOrSuccessOption != c.failureOrSuccessOption,
-      listener: (context, state) {
-        state.failureOrSuccessOption.fold(
-          () => null,
-          (either) => either.fold(
-            (f) => null,
-            (_) {
+    return MultiBlocListener(
+      listeners: [
+        BlocListener<MessageFormBloc, MessageFormState>(
+          listenWhen: (p, c) =>
+              p.failureOrSuccessOption != c.failureOrSuccessOption,
+          listener: (context, state) {
+            state.failureOrSuccessOption.fold(
+              () => null,
+              (either) => either.fold(
+                (f) => null,
+                (_) {
+                  context
+                      .read<MessageFormBloc>()
+                      .add(const MessageFormEvent.dataChanged(''));
+                },
+              ),
+            );
+          },
+        ),
+        BlocListener<RoomWatcherBloc, RoomWatcherState>(
+          listenWhen: (p, c) => p.room != c.room,
+          listener: (context, state) {
+            final room = state.room;
+            if (room.members.isNotEmpty()) {
               context
-                  .read<MessageFormBloc>()
-                  .add(const MessageFormEvent.dataChanged(''));
-            },
-          ),
-        );
-      },
+                  .read<MemberWatcherBloc>()
+                  .add(MemberWatcherEvent.watchAllStarted(room.members));
+            }
+          },
+        ),
+      ],
       child: Scaffold(
         appBar: MyAppBar(
           title: BlocBuilder<RoomWatcherBloc, RoomWatcherState>(
@@ -139,6 +156,9 @@ class RoomPage extends StatelessWidget implements AutoRouteWrapper {
         BlocProvider(
           create: (context) => getIt<MessagesWatcherBloc>()
             ..add(MessagesWatcherEvent.watchAllStarted(roomId)),
+        ),
+        BlocProvider(
+          create: (context) => getIt<MemberWatcherBloc>(),
         ),
       ],
       child: this,
