c7734c0f37e692396d29f6e1ae8756886dd1233a
yudystriawan <yudistriawan@gmail.com>
2023-09-10 22:40:02 +0800

    add extensions

--- core/lib/utils/extensions/date_formatter.dart
+++ core/lib/utils/extensions/date_formatter.dart
@@ -5,4 +5,22 @@ extension DateTimeX on DateTime {
   String toStringFormatted([String? pattern = 'dd MMM yyyy hh:mm']) {
     return DateFormat(pattern).format(this);
   }
+
+  /// format String date time
+  /// return `today`, `yesterday` or `dd/MM/yy`
+  String toStringDate() {
+    final now = DateTime.now();
+
+    final currentDateTime = DateTime(now.year, now.month, now.day);
+
+    final compareDateTime = DateTime(year, month, day);
+
+    if (currentDateTime == compareDateTime) return 'Today';
+
+    if (compareDateTime == currentDateTime.subtract(const Duration(days: 1))) {
+      return 'Yesterday';
+    }
+
+    return toStringFormatted('dd/MM/yy');
+  }
 }
