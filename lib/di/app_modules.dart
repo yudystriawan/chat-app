9522865b67db75d59decda971021ea707639190c
yudystriawan <yudistriawan@gmail.com>
2023-09-14 06:28:12 +0800

    remove unused file

--- lib/di/app_modules.dart
+++
@@ -1,9 +0,0 @@
-// import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:core/core.dart';
-// import 'package:injectable/injectable.dart';
-
-// @module
-// abstract class RegisterModules {
-//   @lazySingleton
-//   FirestoreService get service => FirestoreService(getIt<FirebaseFirestore>());
-// }
