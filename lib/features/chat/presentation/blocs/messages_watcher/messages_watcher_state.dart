132a2653009d76f30b899bf65e25ace13859a0a1
yudystriawan <yudistriawan@gmail.com>
2023-09-16 06:26:31 +0800

    add more data state

--- lib/features/chat/presentation/blocs/messages_watcher/messages_watcher_state.dart
+++ lib/features/chat/presentation/blocs/messages_watcher/messages_watcher_state.dart
@@ -2,11 +2,18 @@ part of 'messages_watcher_bloc.dart';
 
 @freezed
 class MessagesWatcherState with _$MessagesWatcherState {
+  const MessagesWatcherState._();
   const factory MessagesWatcherState({
     required KtList<Message> messages,
     required Option<Failure> failureOption,
     @Default(false) bool isLoading,
+    @Default(1) int currentPage,
+
+    /// show page per size
+    @Default(10) int pageSize,
   }) = _MessagesWatcherState;
+
+  int get limit => currentPage * pageSize;
 
   factory MessagesWatcherState.initial() => MessagesWatcherState(
       messages: const KtList.empty(), failureOption: none());
