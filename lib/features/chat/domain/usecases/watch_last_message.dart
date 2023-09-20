b919a7b7e20c6c6c8fb5d77f9d4fcd30316cce0c
yudystriawan <yudistriawan@gmail.com>
2023-09-20 20:33:45 +0800

    add watch last message usecase

---
+++ lib/features/chat/domain/usecases/watch_last_message.dart
@@ -0,0 +1,30 @@
+import 'package:chat_app/features/chat/domain/reporitories/chat_repository.dart';
+import 'package:core/utils/errors/failure.dart';
+import 'package:core/utils/usecases/usecase.dart';
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+import 'package:injectable/injectable.dart';
+
+import '../entities/entity.dart';
+
+@injectable
+class WatchLastMessage
+    implements StreamUsecase<Message, WatchLastMessageParams> {
+  final ChatRepository _repository;
+
+  WatchLastMessage(this._repository);
+
+  @override
+  Stream<Either<Failure, Message>> call(params) {
+    return _repository.watchLastMessage(params.roomId);
+  }
+}
+
+class WatchLastMessageParams extends Equatable {
+  final String roomId;
+
+  const WatchLastMessageParams(this.roomId);
+
+  @override
+  List<Object> get props => [roomId];
+}
