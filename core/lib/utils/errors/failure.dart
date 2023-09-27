ef1e3a3ae2f7e33e5e899253f34979f44f2dac38
yudystriawan <yudistriawan@gmail.com>
2023-09-28 06:33:51 +0800

    add failure

--- core/lib/utils/errors/failure.dart
+++ core/lib/utils/errors/failure.dart
@@ -10,4 +10,5 @@ class Failure with _$Failure {
   const factory Failure.canceledByUser() = _CanceledByUser;
   const factory Failure.unauthenticated() = _Unauthenticated;
   const factory Failure.notFound() = _NotFound;
+  const factory Failure.invalidParameter({String? message}) = _InvalidParameter;
 }
