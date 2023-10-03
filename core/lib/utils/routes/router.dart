fc84dd1ae5afb88d79a15b1c32cbd14720dff296
yudystriawan <yudistriawan@gmail.com>
2023-10-04 05:51:17 +0800

    add export file

--- core/lib/utils/routes/router.dart
+++ core/lib/utils/routes/router.dart
@@ -1,5 +1,8 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:core/utils/routes/router.gm.dart';
 
+export './guards/auth_guard.dart';
+export './observers/route_observer.dart';
+
 @AutoRouterConfig.module()
 class CoreRouter extends $CoreRouter {}
