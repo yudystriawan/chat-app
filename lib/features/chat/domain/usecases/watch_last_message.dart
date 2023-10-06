c56b588b5201e613f71c2d2f05e1cf619219664d
yudystriawan <yudistriawan@gmail.com>
2023-10-06 23:47:34 +0800

    add failure to usecase

--- lib/features/chat/domain/usecases/watch_last_message.dart
+++ lib/features/chat/domain/usecases/watch_last_message.dart
@@ -1,4 +1,3 @@
-import '../reporitories/chat_repository.dart';
 import 'package:core/utils/errors/failure.dart';
 import 'package:core/utils/usecases/usecase.dart';
 import 'package:dartz/dartz.dart';
@@ -6,6 +5,7 @@ import 'package:equatable/equatable.dart';
 import 'package:injectable/injectable.dart';
 
 import '../entities/entity.dart';
+import '../reporitories/chat_repository.dart';
 
 @injectable
 class WatchLastMessage
@@ -15,8 +15,9 @@ class WatchLastMessage
   WatchLastMessage(this._repository);
 
   @override
-  Stream<Either<Failure, Message>> call(params) {
-    return _repository.watchLastMessage(params.roomId);
+  Stream<Either<Failure, Message>> call(params) async* {
+    if (params.failure != null) yield* Stream.value(left(params.failure!));
+    yield* _repository.watchLastMessage(params.roomId);
   }
 }
 
@@ -24,6 +25,13 @@ class WatchLastMessageParams extends Equatable {
   final String roomId;
 
   const WatchLastMessageParams(this.roomId);
+
+  Failure? get failure {
+    if (roomId.isEmpty) {
+      return const Failure.invalidParameter(message: 'RoomId cannot be empty.');
+    }
+    return null;
+  }
 
   @override
   List<Object> get props => [roomId];
