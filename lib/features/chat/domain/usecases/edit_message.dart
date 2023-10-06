e9de924de8eac8994b79c7dc517bd5dba9fd3481
yudystriawan <yudistriawan@gmail.com>
2023-10-06 23:24:20 +0800

    add edit message unit test

--- lib/features/chat/domain/usecases/edit_message.dart
+++ lib/features/chat/domain/usecases/edit_message.dart
@@ -1,4 +1,3 @@
-import '../reporitories/chat_repository.dart';
 import 'package:core/utils/errors/failure.dart';
 import 'package:core/utils/usecases/usecase.dart';
 import 'package:dartz/dartz.dart';
@@ -6,6 +5,7 @@ import 'package:equatable/equatable.dart';
 import 'package:injectable/injectable.dart';
 
 import '../entities/entity.dart';
+import '../reporitories/chat_repository.dart';
 
 @injectable
 class EditMessage implements Usecase<Unit, EditMessageParams> {
@@ -14,7 +14,9 @@ class EditMessage implements Usecase<Unit, EditMessageParams> {
   EditMessage(this._repository);
 
   @override
-  Future<Either<Failure, Unit>> call(params) {
+  Future<Either<Failure, Unit>> call(params) async {
+    if (params.failure != null) return left(params.failure!);
+
     return _repository.editMessage(
       roomId: params.roomId,
       message: params.message,
@@ -30,6 +32,19 @@ class EditMessageParams extends Equatable {
     required this.roomId,
     required this.message,
   });
+
+  Failure? get failure {
+    if (roomId.isEmpty) {
+      return const Failure.invalidParameter(
+          message: 'Room Id cannot be empty.');
+    }
+
+    if (message == Message.empty()) {
+      return const Failure.invalidParameter(message: 'Message is not valid.');
+    }
+
+    return null;
+  }
 
   @override
   List<Object> get props => [roomId, message];
