5812f201993df60a9a14f0c737fbdc93a6ad28d9
yudystriawan <yudistriawan@gmail.com>
2023-10-06 23:52:02 +0800

    add usecase failure

--- lib/features/chat/domain/usecases/watch_members.dart
+++ lib/features/chat/domain/usecases/watch_members.dart
@@ -1,4 +1,3 @@
-import '../reporitories/chat_repository.dart';
 import 'package:core/utils/errors/failure.dart';
 import 'package:core/utils/usecases/usecase.dart';
 import 'package:dartz/dartz.dart';
@@ -7,6 +6,7 @@ import 'package:injectable/injectable.dart';
 import 'package:kt_dart/collection.dart';
 
 import '../entities/entity.dart';
+import '../reporitories/chat_repository.dart';
 
 @injectable
 class WatchMembers
@@ -16,8 +16,10 @@ class WatchMembers
   WatchMembers(this._repository);
 
   @override
-  Stream<Either<Failure, KtList<Member>>> call(params) {
-    return _repository.watchMembers(params.ids);
+  Stream<Either<Failure, KtList<Member>>> call(params) async* {
+    if (params.failure != null) yield* Stream.value(left(params.failure!));
+
+    yield* _repository.watchMembers(params.ids);
   }
 }
 
@@ -27,6 +29,13 @@ class WatchMembersParams extends Equatable {
   const WatchMembersParams({
     required this.ids,
   });
+
+  Failure? get failure {
+    if (ids.isEmpty()) {
+      return const Failure.invalidParameter(message: 'ids cannot be empty.');
+    }
+    return null;
+  }
 
   @override
   List<Object> get props => [ids];
