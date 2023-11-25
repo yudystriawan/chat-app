6e1ce39f04a0c88bcd88ec00c60458bca37d1813
yudystriawan <yudistriawan@gmail.com>
2023-11-25 13:07:15 +0800

    remove unused code

--- core/lib/features/auth/presentation/blocs/auth/auth_bloc.dart
+++ core/lib/features/auth/presentation/blocs/auth/auth_bloc.dart
@@ -1,15 +1,16 @@
 import 'dart:async';
 
-import 'package:core/features/auth/domain/entities/user.dart';
-import 'package:core/features/auth/domain/usecases/sign_out.dart';
-import 'package:core/features/auth/domain/usecases/watch_current_user.dart';
-import 'package:core/utils/errors/failure.dart';
-import 'package:core/utils/usecases/usecase.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
+
+import '../../../../../utils/errors/failure.dart';
+import '../../../../../utils/usecases/usecase.dart';
+import '../../../domain/entities/user.dart';
+import '../../../domain/usecases/sign_out.dart';
+import '../../../domain/usecases/watch_current_user.dart';
 
 part 'auth_bloc.freezed.dart';
 part 'auth_event.dart';
@@ -20,7 +21,6 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
   final SignOut _signOut;
   final WatchCurrentUser _watchUserAuth;
   final AuthProvider _authProvider;
-  // final GetSignedInUser _getSignedInUser;
 
   StreamSubscription<Either<Failure, User>>? _userAuthSubscription;
 
@@ -30,7 +30,6 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     this._signOut,
     this._watchUserAuth,
     this._authProvider,
-    // this._getSignedInUser,
   ) : super(AuthState.initial()) {
     on<_WatchUserStarted>(_onWatchUserStarted);
     on<_SignOut>(_onSignOut);
