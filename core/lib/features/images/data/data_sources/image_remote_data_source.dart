85cdfb254d1819de23675651bcd501d3cbc6e9d5
yudystriawan <yudistriawan@gmail.com>
2023-12-25 16:40:41 +0800

    fix image data source

--- core/lib/features/images/data/data_sources/image_remote_data_source.dart
+++ core/lib/features/images/data/data_sources/image_remote_data_source.dart
@@ -3,6 +3,7 @@
 import 'dart:io';
 
 import 'package:core/core.dart';
+import 'package:core/services/auth/auth_service.dart';
 import 'package:core/services/firebase_storage/storage.dart';
 import 'package:core/services/firebase_storage/storage_helper.dart';
 import 'package:core/services/firestore/firestore_helper.dart';
@@ -16,16 +17,16 @@ abstract class ImageRemoteDataSource {
 @Injectable(as: ImageRemoteDataSource)
 class ImageRemoteDataSourceImpl implements ImageRemoteDataSource {
   final StorageService _storageService;
-  final FirestoreService _firestoreService;
+  final AuthService _authService;
 
   ImageRemoteDataSourceImpl(
     this._storageService,
-    this._firestoreService,
+    this._authService,
   );
   @override
   Future<String?> uploadImage(File imageFile) async {
     try {
-      final uid = _firestoreService.instance.currentUser!.uid;
+      final uid = _authService.currentUser!.uid;
       String targetPath = 'room_$uid.${p.extension(imageFile.path)}';
 
       final imageRef =
