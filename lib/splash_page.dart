6bd8c6365819dcddbac17cc0294a41e087fc29d5
yudystriawan <yudistriawan@gmail.com>
2024-02-04 15:20:42 +0800

    fix splash screen route

--- lib/splash_page.dart
+++ lib/splash_page.dart
@@ -1,6 +1,10 @@
+import 'dart:developer';
+
 import 'package:auto_route/auto_route.dart';
+import 'package:core/features/auth/presentation/blocs/auth/auth_bloc.dart';
 import 'package:core/styles/typography.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -31,9 +35,6 @@ class _SplashPageState extends State<SplashPage>
     // Create a fade-in animation
     _animation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
 
-    // Start the animation
-    _controller.forward();
-
     // Navigate to the next page after the animation completes
     _controller.addStatusListener((status) {
       if (status == AnimationStatus.completed) {
@@ -44,25 +45,33 @@ class _SplashPageState extends State<SplashPage>
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: FadeTransition(
-          opacity: _animation,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              SvgPicture.asset(
-                'assets/icons/logo.svg',
-                package: 'core',
-                width: 56.w,
-                height: 56.w,
-              ),
-              4.verticalSpaceFromWidth,
-              Text(
-                'Chat App',
-                style: AppTypography.heading2.bold,
-              ),
-            ],
+    return BlocListener<AuthBloc, AuthState>(
+      listenWhen: (p, c) => p.status != c.status,
+      listener: (context, state) {
+        log('status listen: ${state.status}');
+        // Start the animation
+        _controller.forward();
+      },
+      child: Scaffold(
+        body: Center(
+          child: FadeTransition(
+            opacity: _animation,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SvgPicture.asset(
+                  'assets/icons/logo.svg',
+                  package: 'core',
+                  width: 56.w,
+                  height: 56.w,
+                ),
+                4.verticalSpaceFromWidth,
+                Text(
+                  'Chat App',
+                  style: AppTypography.heading2.bold,
+                ),
+              ],
+            ),
           ),
         ),
       ),
