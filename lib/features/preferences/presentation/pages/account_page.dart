595a14fc84c1a24db1d12ed3b109d73d219f1659
yudystriawan <yudistriawan@gmail.com>
2024-02-04 12:08:58 +0800

    fix account page

--- lib/features/preferences/presentation/pages/account_page.dart
+++ lib/features/preferences/presentation/pages/account_page.dart
@@ -1,7 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:core/features/auth/presentation/blocs/auth/auth_bloc.dart';
+import 'package:core/styles/buttons/secondary_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../shared/app_bar.dart';
 
@@ -15,11 +17,15 @@ class AccountPage extends StatelessWidget {
       appBar: const MyAppBar(
         title: Text('Account'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () =>
-              context.read<AuthBloc>().add(const AuthEvent.signOut()),
-          child: const Text('Sign Out'),
+      body: const SizedBox(),
+      bottomNavigationBar: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.w),
+          child: SecondaryButton(
+            onPressed: () =>
+                context.read<AuthBloc>().add(const AuthEvent.signOut()),
+            child: const Text('Sign Out'),
+          ),
         ),
       ),
     );
