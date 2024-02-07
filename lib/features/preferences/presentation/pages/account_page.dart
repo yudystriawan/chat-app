bf8f8a375b756ebd4916d9d293cefd2d3fb26de7
yudystriawan <yudistriawan@gmail.com>
2024-02-07 21:48:53 +0800

    move signout button and add delete account button

--- lib/features/preferences/presentation/pages/account_page.dart
+++ lib/features/preferences/presentation/pages/account_page.dart
@@ -1,8 +1,6 @@
 import 'package:auto_route/auto_route.dart';
-import 'package:core/features/auth/presentation/blocs/auth/auth_bloc.dart';
 import 'package:core/styles/buttons/secondary_button.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../../../shared/app_bar.dart';
@@ -21,10 +19,9 @@ class AccountPage extends StatelessWidget {
       bottomNavigationBar: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 24.w),
-          child: SecondaryButton(
-            onPressed: () =>
-                context.read<AuthBloc>().add(const AuthEvent.signOut()),
-            child: const Text('Sign Out'),
+          child: SecondaryButton.error(
+            onPressed: () {},
+            child: const Text('Delete this account!'),
           ),
         ),
       ),
