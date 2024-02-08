bdf80598a510013d8d3ffa1b2d6fd9fcd51e0ca5
yudystriawan <yudistriawan@gmail.com>
2024-02-08 17:52:55 +0800

    update logic

--- packages/core/lib/features/auth/data/datasources/auth_remote_datasource.dart
+++ packages/core/lib/features/auth/data/datasources/auth_remote_datasource.dart
@@ -57,19 +57,28 @@ class AuthFirebaseDataSource implements AuthRemoteDataSource {
 
   @override
   Stream<UserDto?> watchCurrentUser() {
-    return _authService.watchCurrentUser().switchMap((user) {
+    return _authService.watchCurrentUser().map((user) {
       if (user == null) throw const Failure.unauthenticated();
 
-      return _firestoreService.watch('users', user.uid).map((json) {
-        if (json != null) return UserDto.fromJson(json);
-        return UserDto(
-          id: user.uid,
-          email: user.email,
-          name: user.displayName,
-          phoneNumber: user.phoneNumber,
-          photoUrl: user.photoURL,
-        );
-      });
+      return UserDto(
+        id: user.uid,
+        email: user.email,
+        name: user.displayName,
+        phoneNumber: user.phoneNumber,
+        photoUrl: user.photoURL,
+      );
+
+      // return _firestoreService.watch('users', user.uid).map((json) {
+      //   if (json != null) return UserDto.fromJson(json);
+
+      //   return UserDto(
+      //     id: user.uid,
+      //     email: user.email,
+      //     name: user.displayName,
+      //     phoneNumber: user.phoneNumber,
+      //     photoUrl: user.photoURL,
+      //   );
+      // });
     }).onErrorReturnWith((error, stackTrace) =>
         throw Failure.serverError(message: error.toString()));
   }
